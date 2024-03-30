ea24f12e2756727f639e4c46dd7cc522cd29cc17
amusukwa <amusukwa@gmail.com>
2024-03-30 03:34:43 -0700

    documentation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
 import sys
 import re
 import signal
 
+
 def parse_line(line):
+    """Parse a line of log data."""
     # Regular expression pattern to match the input format
     pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-    
+
     # Match the pattern
     match = re.match(pattern, line)
     if match:
@@ -18,19 +21,24 @@ def parse_line(line):
     else:
         return None
 
+
 def print_statistics(total_file_size, status_code_count):
+    """Print statistics."""
     print("File size:", total_file_size)
     for status_code in sorted(status_code_count.keys()):
         print("{}: {}".format(status_code, status_code_count[status_code]))
 
+
 def signal_handler(sig, frame):
+    """Handle signal interruption."""
     print("\nKeyboard Interruption (CTRL + C) detected. Printing statistics.")
     print_statistics(total_file_size, status_code_count)
     sys.exit(0)
 
+
 def main():
     global total_file_size, status_code_count
-    
+
     total_file_size = 0
     status_code_count = {}
 
@@ -46,15 +54,17 @@ def main():
             if parsed_data:
                 ip_address, date, status_code, file_size = parsed_data
                 total_file_size += file_size
-                status_code_count[status_code] = status_code_count.get(status_code, 0) + 1
+                status_code_count[status_code] = status_code_count.get(
+                    status_code, 0) + 1
 
             # Print statistics after every 10 lines
             if line_count % 10 == 0:
                 print_statistics(total_file_size, status_code_count)
 
     except (KeyboardInterrupt, BrokenPipeError):
-        signal_handler(signal.SIGINT, None)  # Trigger signal handler for CTRL + C
+        # Trigger signal handler for CTRL + C
+        signal_handler(signal.SIGINT, None)
+
 
 if __name__ == "__main__":
     main()
-
