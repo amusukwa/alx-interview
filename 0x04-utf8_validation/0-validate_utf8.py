f5c4811eaa98e26f37a3a2d5d4ecb1ed597905bb
amusukwa <amusukwa@gmail.com>
2024-04-04 13:18:44 -0700

    pycodestyler error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,19 +5,19 @@ UTF-8 Validation file
 
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    """Finds if integer is UTF-8"""
     bytes_to_follow = 0
-    
+
     for num in data:
         if bytes_to_follow == 0:
             # Count the number of bytes to follow based on the first few bits
             if num >> 7 == 0:  # 1 byte character
                 bytes_to_follow = 0
-            elif num >> 5 == 0b110:  # 2 bytes character
+            elif num >> 5 == 0b110:
                 bytes_to_follow = 1
-            elif num >> 4 == 0b1110:  # 3 bytes character
+            elif num >> 4 == 0b1110:
                 bytes_to_follow = 2
-            elif num >> 3 == 0b11110:  # 4 bytes character
+            elif num >> 3 == 0b11110:
                 bytes_to_follow = 3
             else:
                 return False  # Invalid start byte for UTF-8 character
