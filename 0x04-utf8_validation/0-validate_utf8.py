f89eb4973c7c7d06f32eeb7b8446cb5e9fdebb8a
amusukwa <amusukwa@gmail.com>
2024-04-04 12:09:09 -0700

    0-validate_utf8.py error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,37 +5,25 @@ UTF-8 Validation file
 
 
 def validUTF8(data):
-    # Variable to keep track of the number of bytes in the current character
-    num_bytes = 0
-
-    # Iterate through each integer in the data list
+    # Number of bytes in the current UTF-8 character
+    bytes_to_follow = 0
+    
     for num in data:
-        # If num_bytes is 0, it means we are at the start of a new character
-        if num_bytes == 0:
-            # Calculate the number of bytes in the current character based on the first few bits
-            if num >> 7 == 0b0:
-                num_bytes = 1
-            elif num >> 5 == 0b110:
-                num_bytes = 2
-            elif num >> 4 == 0b1110:
-                num_bytes = 3
-            elif num >> 3 == 0b11110:
-                num_bytes = 4
+        if bytes_to_follow == 0:
+            # Count the number of bytes to follow based on the first few bits
+            if num >> 7 == 0:  # 1 byte character
+                bytes_to_follow = 0
+            elif num >> 5 == 0b110:  # 2 bytes character
+                bytes_to_follow = 1
+            elif num >> 4 == 0b1110:  # 3 bytes character
+                bytes_to_follow = 2
+            elif num >> 3 == 0b11110:  # 4 bytes character
+                bytes_to_follow = 3
             else:
-                return False
-
-            if num_bytes == 1:
-                continue
-
-            num_bytes -= 1
-
+                return False  # Invalid start byte for UTF-8 character
         else:
-            if not (num >> 6 == 0b10):
-                return False
+            if num >> 6 != 0b10:
+                return False  # Subsequent bytes must start with '10'
+            bytes_to_follow -= 1
 
-            # Decrement the number of bytes to process in the sequence
-            num_bytes -= 1
-
-    # If all bytes are processed and num_bytes is 0, it's a valid UTF-8 encoding
-    return num_bytes == 0
-
+    return bytes_to_follow == 0
