34704202e6de2e0db80bb0ddfba8d182c0e0b0df
amusukwa <amusukwa@gmail.com>
2024-05-16 06:55:05 +0000

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,3 +11,56 @@ def isWinner(x, nums):
             p += 1
         prime_list = [p for p in range(2, n + 1) if is_prime[p]]
         return prime_list
+    
+    # Find the maximum value of n to consider for the sieve
+    max_n = max(nums)
+    primes = sieve(max_n)
+    
+    def game_winner(n):
+        """ Determine the winner of the game for given n """
+        # Use the precomputed primes to simulate the game
+        primes_set = set(primes)
+        primes_used = set()
+        
+        def can_play(current_n):
+            """ Check if there's a prime that can still be played """
+            for prime in primes_set:
+                if prime not in primes_used and prime <= current_n:
+                    return True
+            return False
+        
+        turn = 0  # 0 for Maria, 1 for Ben
+        current_n = n
+        
+        while can_play(current_n):
+            for prime in primes_set:
+                if prime not in primes_used and prime <= current_n:
+                    primes_used.add(prime)
+                    multiple = prime
+                    while multiple <= current_n:
+                        primes_used.add(multiple)
+                        multiple += prime
+                    break
+            turn = 1 - turn
+        
+        if turn == 1:
+            return "Maria"
+        else:
+            return "Ben"
+    
+    maria_wins = 0
+    ben_wins = 0
+    
+    for n in nums:
+        winner = game_winner(n)
+        if winner == "Maria":
+            maria_wins += 1
+        elif winner == "Ben":
+            ben_wins += 1
+    
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
